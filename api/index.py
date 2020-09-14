5074ec433218bd53406ba68f68547b243aff9a03
Lisa-Stubert <61182572+Lisa-Stubert@users.noreply.github.com>
2020-09-14 17:53:28 +0200

    tidy up python code

--- api/index.py
+++ api/index.py
@@ -6,17 +6,6 @@ import cgi
 
 class handler(BaseHTTPRequestHandler):
 
-    # def do_GET(self):
-    #     message = validation(schema_name, xml)
-    #    # print(message)
-    #     data = {"message": message}
-    #     response = json.dumps(data)
-    #     self.send_response(200)
-    #     self.send_header('Content-type', 'application/json')
-    #     self.end_headers()
-    #     self.wfile.write(response.encode())
-    #     return
-
     def do_POST(self):
         # Parse the form data posted
         form = cgi.FieldStorage(
@@ -26,58 +15,20 @@ class handler(BaseHTTPRequestHandler):
                      'CONTENT_TYPE': self.headers['Content-Type'],
                      })
 
-        # check if xml comes from uploaded file or pasted string
-        file_check = False
-        txt_check = False
-        for field in form.keys():
-            field_item = form[field]
-            if field_item.filename:
-                # The field contains an uploaded file
-                file_data = field_item.file.read()
-                file_len = len(file_data)
-                # print(file_data)
-                file_data = file_data.decode("utf-8")
-                xml = file_data
-                xml = xml.replace("\n", "       ")
-                del file_data
-                file_check = True
-                #self.wfile.write(bytes("file received", "utf-8"))
-            if form.getvalue('txt'):
-                xml = form.getvalue('txt')
-                # print(xml)
-                txt_check = True
-        
-        if file_check and txt_check:
+        xml = form.getvalue('txt')     
+
+        try:
+            schema_name = form.getvalue('schemas')
+            message = validation(schema_name, xml)
+        except:
             message = {"status": "error",
-                "message":'Bitte machen Sie nur eine Eingabe. Laden Sie entweder eine Datei hoch oder kopieren Sie die GML-Datei in das Textfeld.'}
-        else:
-            try:
-                schema_name = form.getvalue('schemas')
-                message = validation(schema_name, xml)
-            except:
-                message = {"status": "error",
-                    "message":'Keine Datei zum Validieren oder ungültige Datei. Laden Sie entweder eine Datei hoch oder kopieren Sie die GML-Datei in das Textfeld.'}
+                "message":'Keine Datei zum Validieren oder ungültige Datei. Laden Sie entweder eine Datei hoch oder kopieren Sie die GML-Datei in das Textfeld.'}
 
         # Begin the response
         self.send_response(201)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         data = message
-        # Echo back information about what was posted in the form
-        # for field in form.keys():
-        #  field_item = form[field]
-        #  print(field)
-        # print(field_item)
-        # if field_item.filename:
-        #     # The field contains an uploaded file
-        #     file_data = field_item.file.read()
-        #     file_len = len(file_data)
-        #     print(file_data)
-        #     del file_data
-        #     self.wfile.write(bytes("file received", "utf-8"))
-        # else:
-        #     # Regular form value
-        #     print(field, form[field].value)
 
         self.wfile.write(bytes(json.dumps(data), "utf-8"))
         return
